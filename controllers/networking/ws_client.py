9aa583023f6f63a626368feabd24c39c098f06a1
Ahmed Karem Mousa <akaremmousa@gmail.com>
2026-02-24 20:00:50 +0200

    chore: req-repl print statements

--- controllers/networking/ws_client.py
+++ controllers/networking/ws_client.py
@@ -21,6 +21,7 @@ async def read_handler(websocket):
                     data = json.loads(message)
                     msg_data = data.get("message")
                     msg_type = data.get("msg_type")
+                    print("Server: Recieved msg type: ", msg_type)
                     # If the message type is for changing the secret key
                     if msg_type == "ChangeSecret":
                         secret_data = SecretMetadataKey(**msg_data)
